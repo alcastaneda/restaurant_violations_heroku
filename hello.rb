2a54066797e38c83b812335065c9feec7c278212
Adriana Castaneda <adriana.lcs316@gmail.com>
2018-04-27 10:50:23 -0500

    add find_restaurants function and results page

--- hello.rb
+++ hello.rb
@@ -1,9 +1,23 @@
 require 'sinatra'
+require 'httparty'
+
+def find_restaurants(params)
+	request = HTTParty.get('https://data.cityofchicago.org/resource/cwig-ma7x.json', 
+		{headers: {"X-App-Token": ENV['SODA_API_KEY']},
+		query: sanitized_params})
+	if request.code == 200 && request.parsed_response != []
+		results = format_results(request)
+		return results
+	else
+		return "Something went wrong. Check your inputs."
+	end
+end
 
 get '/' do
   erb :search
 end
 
 get	'/results' do
-
+	@results = find_restaurants(params[:restaurant])
+	erb :results
 end
